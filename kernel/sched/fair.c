wloot/android_kernel_xiaomi_msm8998

sched/fair: Re-allow negative boost
* also fix distinct types in boosted_cpu_util

kernel/sched/fair.c
@@ -6463,36 +6463,40 @@ unsigned long
 boosted_cpu_util(int cpu)
 {
 	unsigned long util = cpu_util_freq(cpu);
-	long margin = schedtune_cpu_margin(util, cpu);
+	int margin;
 
-	trace_sched_boost_cpu(cpu, util, margin);
-	
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	if (dsb_boosting || sched_feat(SCHEDTUNE_BOOST_UTIL))
 #else
 	if (sched_feat(SCHEDTUNE_BOOST_UTIL))
 #endif
-		return util + margin;
+		margin = 0;
 	else
-		return util;
+		margin = min(0, schedtune_cpu_margin(util, cpu));
+
+	trace_sched_boost_cpu(cpu, util, margin);
+
+	return util + margin;
 }
 
 static inline unsigned long
 boosted_task_util(struct task_struct *task)
 {
 	unsigned long util = task_util_est(task);
-	long margin = schedtune_task_margin(task);
-
-	trace_sched_boost_task(task, util, margin);
+	long margin;
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	if (dsb_boosting || sched_feat(SCHEDTUNE_BOOST_UTIL))
 #else
 	if (sched_feat(SCHEDTUNE_BOOST_UTIL))
 #endif
-		return util + margin;
+		margin = 0;
 	else
-		return util;
+		margin = min((long)0, schedtune_task_margin(task));
+
+	trace_sched_boost_task(task, util, margin);
+
+	return util + margin;
 }
 
 static unsigned long cpu_util_without(int cpu, struct task_struct *p);

YYQ97/kernel_xiaomi_sagit

kernel/sched/fair.c
@@ -6320,36 +6320,40 @@ unsigned long
 boosted_cpu_util(int cpu)
 {
 	unsigned long util = cpu_util_freq(cpu);
-	long margin = schedtune_cpu_margin(util, cpu);
+	int margin;
 
-	trace_sched_boost_cpu(cpu, util, margin);
-	
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	if (dsb_boosting || sched_feat(SCHEDTUNE_BOOST_UTIL))
 #else
 	if (sched_feat(SCHEDTUNE_BOOST_UTIL))
 #endif
-		return util + margin;
+		margin = 0;
 	else
-		return util;
+		margin = min(0, schedtune_cpu_margin(util, cpu));
+
+	trace_sched_boost_cpu(cpu, util, margin);
+
+	return util + margin;
 }
 
 static inline unsigned long
 boosted_task_util(struct task_struct *p)
 {
 	unsigned long util = task_util(p);
-	long margin = schedtune_task_margin(p);
-
-	trace_sched_boost_task(p, util, margin);
+	long margin;
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	if (dsb_boosting || sched_feat(SCHEDTUNE_BOOST_UTIL))
 #else
 	if (sched_feat(SCHEDTUNE_BOOST_UTIL))
 #endif
-		return util + margin;
+		margin = 0;
 	else
-		return util;
+		margin = min((long)0, schedtune_task_margin(p));
+
+	trace_sched_boost_task(p, util, margin);
+
+	return util + margin;
 }
 
 static unsigned long capacity_spare_without(int cpu, struct task_struct *p)